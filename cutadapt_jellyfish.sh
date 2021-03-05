f63f062ff94c36c4a1e84bc84de0cec67c92c4d2
kmeaton <kme0038@auburn.edu>
2021-03-05 18:23:26 -0500

    Added script used to process reads with cutadapt and run jellyfish size estimation

---
+++ cutadapt_jellyfish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#run cutadapt to remove illumina adapters
+cutadapt -a AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT -A GATCGGAAGAGCACACGTCTGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG --trim-n -q 10 --cores=20 -o adapter_trimmed_mate1.fq.gz -p adapter_trimmed_mate2.fq.gz all_mate1.fq.gz all_mate2.fq.gz
+
+#run jellyfish to estimate genome size
+jellyfish count -t 20 -C -m 21 -s 100G --min-quality=20 all_adapter_trimmed_for_jellyfish.fq 
+
